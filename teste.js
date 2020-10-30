b10804259054e52f5135b6d0ab4e724589dead7d
andrechuka <andremiguelchuka@gmail.com>
2020-10-30 17:08:55 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,5 @@
+let x=1;
+console.log ("===========================================")
+console.log("Progama do ANDRE")
+console.log("============================================")
+console.log (x+2);
